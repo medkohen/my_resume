bb63cc242015ef3a5c68f2b29445666bc3a5197c
medkohen <simo.kohen1@gmail.com>
2024-10-22 18:33:46 +0200

    refactor js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,21 @@
 // Animations
 AOS.init({
-  anchorPlacement: 'top-left',
-  duration: 1000
+  anchorPlacement: "top-left",
+  duration: 1000,
 });
 
-// Add your javascript here
+// collapse script
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active_collapse");
+    var content = this.nextElementSibling;
+    if (content.style.display === "flex") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "flex";
+    }
+  });
+}
